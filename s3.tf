bc57cb063bb4b73a9eb239dec81fa593da322e3e
Asthana <ankit.asthana@capgemini.com>
2020-01-12 00:35:30 +0100

    Updated S3, terraform, example.tf

---
+++ s3.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "terraform-state-storage-s3" {
+    bucket = "terraform-remote-state-storage-s3"
+ 
+    versioning {
+      enabled = true
+    }
+ 
+    lifecycle {
+      prevent_destroy = true
+    }
+ 
+  }
+    tags {
+      Name = "S3 Remote Terraform State Store"
+    }      
+}
